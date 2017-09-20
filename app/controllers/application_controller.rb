9d89953513186a69b3e0c7358c17539c06269471
stopvoldemort <david.squires1@gmail.com>
2017-09-20 15:37:56 -0400

    added views and blank routes to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,60 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :new
+  end
+
+
+  get '/login' do
+    erb :login
+  end
+
+  get '/logout' do
+    erb :logout
+  end
+
+
+  get '/tweets' do
+    # Grab all tweets
+    erb :index
+  end
+
+  get '/tweets/new' do
+    erb :new
+  end
+
+  post '/tweets' do
+    # Receive post from '/tweets/new'
+    redirect to '/tweets'
+  end
+
+  get '/tweets/:id' do
+    # Grab instance of tweet
+    erb :show
+  end
+
+  get '/tweets/:id/edit' do
+    # Grab instance of tweet
+    erb :edit
+  end
+
+  patch '/tweets/:id' do
+    # Receive patch from '/tweets/:id/edit'
+    erb "/tweets/#{@tweet.id}"
+  end
+
+  delete '/tweets/:id' do
+    # Receive delete from '/tweets/:id'
+    redirect to '/tweets'
+  end
+
+
+
 end
